9c472c0221968ffd5079ada6aba6547ef6a06459
jane-tsc <lanren@192.168.0.9>
2017-03-23 11:20:25 +0800

    twoCommit
    啊实打实

---
+++ theNncTown/class/home/testView.h
@@ -0,0 +1,13 @@
+//
+//  testView.h
+//  theNncTown
+//
+//  Created by lanren on 17/3/23.
+//  Copyright © 2017年 com.nnctown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testView : UIView
+
+@end
---
+++ theNncTown/class/home/testView.m
@@ -0,0 +1,21 @@
+//
+//  testView.m
+//  theNncTown
+//
+//  Created by lanren on 17/3/23.
+//  Copyright © 2017年 com.nnctown. All rights reserved.
+//
+
+#import "testView.h"
+
+@implementation testView
+
+- (instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
